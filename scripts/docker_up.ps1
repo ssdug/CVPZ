d40c303eee0d5f5ec1c7cf3aafebb3dd90a61ed0
Carter Barnes <carterabarnes@gmail.com>
2017-10-29 16:33:12 -0700

    Add a rebuild web only docker-compose and script option

--- scripts/docker_up.ps1
+++ scripts/docker_up.ps1
@@ -2,11 +2,17 @@ param(
     [Alias('r')]
     [switch]$rebuild,
     [Alias('b')]
-    [switch]$background
+    [switch]$background,
+    [Alias('w')]
+    [switch]$web
 )
 
 if($rebuild) {
- docker-compose -f .\docker-compose.ci.build.yml up
+    docker-compose -f .\docker-compose.ci.build.yml up
+}
+
+if ($web) {
+    docker-compose -f .\docker-compose.web.build.yml up
 }
 
 if ($background) {
